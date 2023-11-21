0a2fe749edf637b2d1166a76b898986e8ddea47e
“donaldcampbelljr” <125581724+donaldcampbelljr@users.noreply.github.com>
2023-11-21 12:38:33 -0500

    add bogus data and chart, chunks for regions of terminal

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,24 @@ use crate::iss::{get_position, Iss};
 use std::{thread, time};
 
 
-use ratatui::{
-    prelude::{CrosstermBackend, Terminal},
-    widgets::Paragraph,
-};
+// use ratatui::{
+//     prelude::{CrosstermBackend, Terminal},
+//     widgets::Paragraph,
+// };
+
+use ratatui::{prelude::*, widgets::*};
 
 pub mod iss;
+
+const ISS_DATA: [(f64, f64); 7] = [
+    (-3901.831067378710, -5313.183806503490),
+    (-4579.71120418877, -5028.721419956560),
+    (-4926.24313981341, -4379.407757890590),
+    (-4915.8195182088, -3411.881627490280),
+    (-4548.5594695655, -2196.010698574360),
+    (-3850.5423196244, -820.025684187015),
+    (-2872.12994324591, 615.821403372990),
+];
 
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -28,6 +40,33 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Initialize the terminal backend using crossterm
     let mut terminal = Terminal::new(CrosstermBackend::new(std::io::stderr()))?;
 
+    let datasets = vec![Dataset::default()
+        .name("data")
+        .marker(symbols::Marker::Braille)
+        .style(Style::default().fg(Color::Yellow))
+        .graph_type(GraphType::Line)
+        .data(&ISS_DATA)];
+    // let chart = Chart::new(datasets)
+    //     .block(
+    //         Block::default()
+    //             .title("Chart 3".cyan().bold())
+    //             .borders(Borders::ALL),
+    //     )
+    //     .x_axis(
+    //         Axis::default()
+    //             .title("X Axis")
+    //             .style(Style::default().fg(Color::Gray))
+    //             .bounds([0.0, 50.0])
+    //             .labels(vec!["0".bold(), "25".into(), "50".bold()]),
+    //     )
+    //     .y_axis(
+    //         Axis::default()
+    //             .title("Y Axis")
+    //             .style(Style::default().fg(Color::Gray))
+    //             .bounds([0.0, 5.0])
+    //             .labels(vec!["0".bold(), "2.5".into(), "5".bold()]),
+    //     );
+
     // Define our counter variable
     // This is the state of our application
     let mut counter = 0;
@@ -36,7 +75,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     loop {
         // Render the UI
         terminal.draw(|f| {
-            f.render_widget(Paragraph::new(format!("Counter: {counter}\n\n ISS Position: {0}  {1}  {2}", iss.lat, iss.lon, iss.alt)), f.size());
+            let size = f.size();
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints([
+                    Constraint::Ratio(1, 3),
+                    Constraint::Ratio(1, 3),
+                    Constraint::Ratio(1, 3),
+                ])
+                .split(size);
+            f.render_widget(Chart::new(datasets.clone())
+                                .block(           Block::default()
+                                                      .title("ISS Historical Position".cyan().bold())
+                                                      .borders(Borders::ALL),)
+                                .x_axis(            Axis::default()
+                                                        .title("X Axis")
+                                                        .style(Style::default().fg(Color::Gray))
+                                                        .bounds([-4000.0, -3000.0])
+                                                        .labels(vec!["0".bold(), "25".into(), "50".bold()]),)
+                                .y_axis(            Axis::default()
+                                                        .title("Y Axis")
+                                                        .style(Style::default().fg(Color::Gray))
+                                                        .bounds([-5000.0, 500.0])
+                                                        .labels(vec!["0".bold(), "2.5".into(), "5".bold()]),), chunks[0]);
+            f.render_widget(Paragraph::new(format!("Counter: {counter}\n\n ISS Position: {0}  {1}  {2}", iss.lat, iss.lon, iss.alt)), chunks[1]);
         })?;
 
         // Check for user input every 250 milliseconds
