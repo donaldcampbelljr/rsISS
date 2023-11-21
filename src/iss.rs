224de6e213c9b1d88526a2a215980f3225997bfe
“donaldcampbelljr” <125581724+donaldcampbelljr@users.noreply.github.com>
2023-11-21 11:42:14 -0500

    new api call so that the program can obtain altitude

--- src/iss.rs
+++ src/iss.rs
@@ -4,6 +4,7 @@ use serde_json::{Value};
 pub struct Iss {
     pub lat: f64,
     pub lon: f64,
+    pub alt: f64,
 }
 
 impl Iss {
@@ -17,43 +18,33 @@ impl Iss {
         let new_position = get_position().unwrap();
         self.lat = new_position.0;
         self.lon = new_position.1;
+        self.alt = new_position.2;
     }
+    // fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    //     write!(f, "ISS {{ latitude: {}, longitude: {} }}", self.lat, self.lon)
+    // }
+
 }
 
 
-pub fn get_position() -> Result<(f64,f64), Box<dyn std::error::Error>> {
+pub fn get_position() -> Result<(f64,f64, f64), Box<dyn std::error::Error>> {
 
-    let mut res = reqwest::blocking::get("http://api.open-notify.org/iss-now.json")?;
+    //let mut res = reqwest::blocking::get("http://api.open-notify.org/iss-now.json")?;
+    // https://api.wheretheiss.at/v1/satellites/25544
+    let mut res = reqwest::blocking::get("https://api.wheretheiss.at/v1/satellites/25544")?;
     let mut body = String::new();
     res.read_to_string(&mut body)?;
-    println!("Status: {}", res.status());
-    println!("Headers:\n{:#?}", res.headers());
-    println!("Body:\n{}", body);
 
-    //let json: Value = serde_json::from_str(&body)?;
     let json: Value = match serde_json::from_str(&body) {
         Ok(json) => json,
         Err(err) => return Err(Box::new(err)),
     };
 
-    println!("The message  {} at the timestamp {}", json["message"], json["timestamp"]);
-    println!("iss_position is {}", json["iss_position"]);
-    println!("Latitude is {}", json["iss_position"]["latitude"]);
-    println!("Longitude is {:?}", json["iss_position"]["longitude"]);
-    //println!("Longitude is {:?}", json["iss_position"]["longitude"].as_number()); // THIS RETURNS None
+    let latitude: f64 = json["latitude"].as_f64().expect("Desire a number");
+    let longitude: f64 = json["longitude"].as_f64().expect("Desire a number");
+    let altitude: f64 = json["altitude"].as_f64().expect("Desire a number");
+        // .as_str().expect("str expected")
+        // .parse().expect("Desire a number");
 
-    // for line below Take the JSON Value (which is actually a String()?), convert to &str (make sure to do expectation handling), parse the &str, doing expectation handling
-    // pub fn as_str(&self) -> Option<&str>, If the Value is a String, returns the associated str. Returns None otherwise.
-    let latitude: f64 = json["iss_position"]["latitude"]
-        .as_str().expect("str expected") //https://docs.rs/serde_json/latest/serde_json/value/enum.Value.html#method.as_str // returns str or None
-        .parse().expect("Desire a number"); // parse needs to know what string it is expecting in this cae its an f64 // will return Err if it cannot parse.
-
-    let longitude: f64 = json["iss_position"]["longitude"]
-        .as_str().expect("str expected")
-        .parse().expect("Desire a number");
-
-    println!("Latitude is {:?}", latitude);
-    println!("Longitude is {:?}", longitude);
-    //Ok(())
-    Ok((latitude,longitude))
+    Ok((latitude,longitude, altitude))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,67 @@
 use crate::iss::{get_position, Iss};
 use std::{thread, time};
 
+
+use ratatui::{
+    prelude::{CrosstermBackend, Terminal},
+    widgets::Paragraph,
+};
+
 pub mod iss;
 
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
-
+    //
+    //
     let mut iss = Iss::new();
-    let delay = time::Duration::from_secs(2);
-    thread::sleep(delay);
+    // let delay = time::Duration::from_secs(2);
+    // thread::sleep(delay);
     iss.update_position();
+    //
+    // println!("ENDING PROGRAM");
 
-    println!("ENDING PROGRAM");
+    // startup: Enable raw mode for the terminal, giving us fine control over user input
+    crossterm::terminal::enable_raw_mode()?;
+    crossterm::execute!(std::io::stderr(), crossterm::terminal::EnterAlternateScreen)?;
+
+    // Initialize the terminal backend using crossterm
+    let mut terminal = Terminal::new(CrosstermBackend::new(std::io::stderr()))?;
+
+    // Define our counter variable
+    // This is the state of our application
+    let mut counter = 0;
+
+    // Main application loop
+    loop {
+        // Render the UI
+        terminal.draw(|f| {
+            f.render_widget(Paragraph::new(format!("Counter: {counter}\n\n ISS Position: {0}  {1}  {2}", iss.lat, iss.lon, iss.alt)), f.size());
+        })?;
+
+        // Check for user input every 250 milliseconds
+        if crossterm::event::poll(std::time::Duration::from_millis(250))? {
+            // If a key event occurs, handle it
+            if let crossterm::event::Event::Key(key) = crossterm::event::read()? {
+                if key.kind == crossterm::event::KeyEventKind::Press {
+                    match key.code {
+                        crossterm::event::KeyCode::Char('j') => counter += 1,
+                        crossterm::event::KeyCode::Char('k') => counter -= 1,
+                        crossterm::event::KeyCode::Char('u') => iss.update_position(),
+                        crossterm::event::KeyCode::Char('q') => break,
+                        _ => {},
+                    }
+                }
+            }
+        }
+    }
+
+    // shutdown down: reset terminal back to original state
+    crossterm::execute!(std::io::stderr(), crossterm::terminal::LeaveAlternateScreen)?;
+    crossterm::terminal::disable_raw_mode()?;
+
+    Ok(())
+
+
+
 }
